8c557e630154040bb420365ed4a2c7a6f034711b
zhengyifeng <zhengyifeng@deepexi.com>
2020-07-18 21:17:46 +0800

    feat: fix build

--- boot-dubbo-consumer/src/main/java/com/zhengyf/controller/TestController.java
+++ boot-dubbo-consumer/src/main/java/com/zhengyf/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.zhengyf.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.zhengyf.domain.User;
 import com.zhengyf.service.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
